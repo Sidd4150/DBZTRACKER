8d2d67173453fcad54b768f3b050482168143bf2
Siddhartha Shakya <sshakya@dons.usfca.edu>
2026-01-06 12:01:38 -0800

    simple frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+
+    <div className="h-screen bg-cover bg-center bg-dragonball flex items-center justify-center" style={{ backgroundImage: "url('/bg-dbz-balls.jpg')" }}>
+      <p className="text-3xl font-bold">DBZ Tracker</p>
+    </div>
+  )
+}
+
+export default App
