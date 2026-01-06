8b9241665603bd7919705a89400c7f0ed17071d2
Siddhartha Shakya <sshakya@dons.usfca.edu>
2026-01-06 13:54:18 -0800

    Simple day tracker (needs to change)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,33 @@
 
 
 function App() {
+
+  const pastDate = new Date("2018-03-25"); // Your 25-Mar-18 date
+  const today = new Date();
+
+  // The Subtraction
+  const diffInMs = today - pastDate;
+
+  // Conversion
+  const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+
   return (
 
-    <div className="h-screen bg-cover bg-center bg-dragonball flex items-center justify-center" style={{ backgroundImage: "url('/bg-dbz-balls.jpg')" }}>
-      <p className="text-3xl font-bold">DBZ Tracker</p>
-    </div>
+    <div className="bg-[url('/bg-dbz-balls.jpg')] bg-cover h-screen flex items-center justify-center">
+      <div className="bg-black/60 text-white p-8 rounded-xl shadow-lg max-w-md w-full text-center">
+        <h1 className="text-2xl font-bold">DBZTRACKER</h1>
+
+        <p className="text-5xl font-black text-orange-500">
+          {diffInDays} Days
+        </p>
+
+        <p className="mt-2 text-sm">
+          since the end of the DragonBall Super anime.
+        </p>
+
+
+      </div>
+    </div >
   )
 }
 
